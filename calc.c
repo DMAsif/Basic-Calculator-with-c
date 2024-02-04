94e8e06fa4a180f6e4d1f2f2d3be87e872667258
DMAsif <140385639+DMAsif@users.noreply.github.com>
2024-02-04 13:14:39 +0600

    Add files via upload

---
+++ calc.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main(){
+    float num1, num2, result;
+    char operator;
+    printf("Enter an expression: ");
+
+    scanf("%f%c%f", &num1, &operator, &num2);
+
+
+
+
+
+    if(operator == '+'){
+        result = num1 + num2;
+    } else if(operator == '-') {
+        result = num1 + num2;
+    } else if(operator == '') {
+        result = num1 num2;
+    } else if(operator == '/') {
+        if(num2 != 0) {
+           result = num1 / num2;
+        } else {
+            printf("Error!!!");
+           return 1; 
+
+        }
+
+    }
+    else{
+        printf("Invalid operator!!!");
+        return 1;
+    }
+    printf("Result: %.2f\n", result);
+
+
+    return 0;
+}
